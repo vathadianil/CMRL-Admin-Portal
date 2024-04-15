b4d26a5083ac81e7aaaa01dbd946a07f87c2a325
Anil <anil.kumar@shellinfoglobal.com>
2024-04-15 16:26:44 +0530

    Fab buttons styles changed

--- src/components/fab-button-field/fab-button-field.component.ts
+++ src/components/fab-button-field/fab-button-field.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,8 +8,19 @@ import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-fab-button',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatMenuModule, MatDividerModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatDividerModule,
+    CommonModule,
+  ],
   templateUrl: './fab-button-field.component.html',
   styleUrls: ['./fab-button-field.component.scss'],
 })
-export class FabButtonFieldComponent {}
+export class FabButtonFieldComponent {
+  @Input() icon = 'download';
+  @Input() showMenu = true;
+  @Input() showPdf = true;
+  @Input() isDownloadBtn = true;
+}
