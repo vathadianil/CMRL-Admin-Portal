3fe60dd8cfbce3115f3142aa92f46fa2709a376e
Anil <anil.kumar@shellinfoglobal.com>
2024-04-25 10:47:48 +0530

    Mouse events like zoom and pan events added to map

--- src/pages/monitoring/monitoring.component.ts
+++ src/pages/monitoring/monitoring.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { MouseDownOnMapDirective } from '../../directives/map/mouse-down-on-map.directive';
+import { MatCardModule } from '@angular/material/card';
+import { MouseEventsOnMapDirective } from '../../directives/mouse-events-on-map.directive';
 
 @Component({
   selector: 'app-monitoring',
   standalone: true,
-  imports: [MouseDownOnMapDirective],
+  imports: [MatCardModule, MouseEventsOnMapDirective],
   templateUrl: './monitoring.component.html',
   styleUrl: './monitoring.component.scss',
 })
-export class MonitoringComponent {
-  scale = 1;
-  panning = false;
-  pointX = 0;
-  pointY = 0;
-  start = { x: 0, y: 0 };
-}
+export class MonitoringComponent {}
