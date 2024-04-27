67bffe2952e9391b0b3e6fdd289f790d66e8b933
Anil <anil.kumar@shellinfoglobal.com>
2024-04-27 11:45:27 +0530

    Removed unnecessary lottie files

--- src/components/gate/gate-direction/gate-direction.component.ts
+++ src/components/gate/gate-direction/gate-direction.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { LottieAnimationComponent } from '../../lottie-animation/lottie-animation.component';
 
 @Component({
   selector: 'app-gate-direction',
   standalone: true,
-  imports: [MatIconModule, CommonModule, LottieAnimationComponent],
+  imports: [CommonModule, LottieAnimationComponent],
   templateUrl: './gate-direction.component.html',
   styleUrl: './gate-direction.component.scss',
 })
