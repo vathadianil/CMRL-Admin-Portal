3f855be7bce2c07c7b130013f0e1b690888581e6
Anil <anil.kumar@shellinfoglobal.com>
2024-04-29 15:55:31 +0530

    Switched from google icons to font awesome icons

---
+++ src/util/font-awesome-icons.ts
@@ -0,0 +1,35 @@
+import {
+  faHome,
+  faDesktop,
+  faFile,
+  faMicrochip,
+  faTrain,
+  faUserGear,
+  faUserGroup,
+  faUserTag,
+  faUserShield,
+} from '@fortawesome/free-solid-svg-icons';
+export function getIcon(icon: string): any {
+  let fontawesomeIcon = faHome;
+  switch (icon) {
+    case 'home':
+      return (fontawesomeIcon = faHome);
+    case 'monitoring':
+      return (fontawesomeIcon = faDesktop);
+    case 'file':
+      return (fontawesomeIcon = faFile);
+    case 'microChip':
+      return (fontawesomeIcon = faMicrochip);
+    case 'train':
+      return (fontawesomeIcon = faTrain);
+    case 'manage_accounts':
+      return (fontawesomeIcon = faUserGear);
+    case 'group':
+      return (fontawesomeIcon = faUserGroup);
+    case 'roles':
+      return (fontawesomeIcon = faUserTag);
+    case 'privileges':
+      return (fontawesomeIcon = faUserShield);
+  }
+  return fontawesomeIcon;
+}
