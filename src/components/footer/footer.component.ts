09e9d22708c829858ca541b35a9d2119580307df
a.jyothi@shellinfoglobal.com <a.jyothi@shellinfoglobal.com>
2024-04-30 16:09:22 +0530

    Icon Changed for password and copyrights

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { getIcon } from '../../util/font-awesome-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  getIcon = getIcon;
+}
