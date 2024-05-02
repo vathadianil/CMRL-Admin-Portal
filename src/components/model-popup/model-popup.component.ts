cae2eed28ada8c970f0b73c419b6da54dc859f00
a.jyothi@shellinfoglobal.com <a.jyothi@shellinfoglobal.com>
2024-05-02 17:02:30 +0530

    Changes

--- src/components/model-popup/model-popup.component.ts
+++ src/components/model-popup/model-popup.component.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import {
-  MatDialog,
   MatDialogModule,
   MatDialogRef,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
-
 import { TableComponent } from '../table/table.component';
 import { MatTableDataSource } from '@angular/material/table';
 import { InputTextComponent } from '../input-text/input-text.component';
@@ -48,8 +46,4 @@ export class ModelPopupComponent {
       dataSource: new MatTableDataSource<any>([this.tableData]),
     },
   ];
-
-  btnClick() {
-    console.log(this.data);
-  }
 }
