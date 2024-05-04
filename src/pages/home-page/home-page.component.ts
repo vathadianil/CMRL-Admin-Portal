594055fdec2fd39b5b565969692ca5ba471e6b07
Anil <anil.kumar@shellinfoglobal.com>
2024-05-04 11:08:06 +0530

    Header seperated from home component

--- src/pages/home-page/home-page.component.ts
+++ src/pages/home-page/home-page.component.ts
@@ -17,6 +17,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { getIcon } from '../../util/font-awesome-icons';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-home-page',
@@ -35,6 +36,7 @@ import { getIcon } from '../../util/font-awesome-icons';
     MatExpansionModule,
     MatListModule,
     MatTooltipModule,
+    HeaderComponent,
     FooterComponent,
     FontAwesomeModule,
   ],
@@ -189,7 +191,6 @@ export class HomePageComponent {
           label: 'Over Stay Configuration',
           route: 'OverStayConfiguration',
         },
-      
       ],
     },
   ];
