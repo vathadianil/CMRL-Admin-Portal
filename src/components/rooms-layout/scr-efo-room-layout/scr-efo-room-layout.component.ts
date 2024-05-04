cbe61122cfed544dce089053b8d120b934577f18
Anil <anil.kumar@shellinfoglobal.com>
2024-05-04 13:46:01 +0530

    minor change

--- src/components/rooms-layout/scr-efo-room-layout/scr-efo-room-layout.component.ts
+++ src/components/rooms-layout/scr-efo-room-layout/scr-efo-room-layout.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 import { TomComponent } from '../tom/tom.component';
 import { CommonModule } from '@angular/common';
 import { TrComponent } from '../tr/tr.component';
-import { MatCardModule } from '@angular/material/card';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { getIcon } from '../../../util/font-awesome-icons';
 import { LottieAnimationComponent } from '../../lottie-animation/lottie-animation.component';
@@ -14,7 +13,6 @@ import { LottieAnimationComponent } from '../../lottie-animation/lottie-animatio
     TomComponent,
     CommonModule,
     TrComponent,
-    MatCardModule,
     FontAwesomeModule,
     LottieAnimationComponent,
   ],
