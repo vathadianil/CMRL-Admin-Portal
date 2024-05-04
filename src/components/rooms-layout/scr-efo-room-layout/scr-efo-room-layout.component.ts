6708b345642339bd1a3590d61f8f30227e1b0b6b
Anil <anil.kumar@shellinfoglobal.com>
2024-05-04 13:57:03 +0530

    You are here image added in scr efo component

--- src/components/rooms-layout/scr-efo-room-layout/scr-efo-room-layout.component.ts
+++ src/components/rooms-layout/scr-efo-room-layout/scr-efo-room-layout.component.ts
@@ -2,24 +2,16 @@ import { Component, Input } from '@angular/core';
 import { TomComponent } from '../tom/tom.component';
 import { CommonModule } from '@angular/common';
 import { TrComponent } from '../tr/tr.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { getIcon } from '../../../util/font-awesome-icons';
 import { LottieAnimationComponent } from '../../lottie-animation/lottie-animation.component';
 
 @Component({
   selector: 'app-scr-efo-room-layout',
   standalone: true,
-  imports: [
-    TomComponent,
-    CommonModule,
-    TrComponent,
-    FontAwesomeModule,
-    LottieAnimationComponent,
-  ],
+  imports: [TomComponent, CommonModule, TrComponent, LottieAnimationComponent],
   templateUrl: './scr-efo-room-layout.component.html',
   styleUrl: './scr-efo-room-layout.component.scss',
 })
 export class ScrEfoRoomLayoutComponent {
   @Input() scrEfoRoomData: any = [];
-  getIcon = getIcon;
 }
