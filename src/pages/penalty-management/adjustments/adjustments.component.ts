127f9f3d87b7c08425289205fe45e5f0d17cf903
manikanta <manikantaswamyyalla99@gmail.com>
2024-05-06 15:03:50 +0530

    Penalty-Management Adjustments Search Filter

--- src/pages/penalty-management/adjustments/adjustments.component.ts
+++ src/pages/penalty-management/adjustments/adjustments.component.ts
@@ -6,16 +6,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PagetitleComponent } from '../../../components/pageTitle/page-title.component';
 import { getIcon } from '../../../util/font-awesome-icons';
 import { ButtonFieldComponent } from '../../../components/button-field/button-field.component';
-import { FabButtonFieldComponent } from "../../../components/fab-button-field/fab-button-field.component";
+import { FabButtonFieldComponent } from '../../../components/fab-button-field/fab-button-field.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { SearchComponent } from '../../../components/search/search.component';
 import { TableComponent } from '../../../components/table/table.component';
-import { ExportPdfService } from '../../../services/export-pdf.service';
-import { FormControl, } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonService } from '../../../services/common.service';
-import { ExportService } from '../../../services/export.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { AdjustmentInterface } from '../../../models/Adjustments';
 import { exportAdjustmentsData } from '../../export-data';
@@ -25,41 +23,41 @@ import { AdjustmentsData } from '../../sample';
   selector: 'app-adjustments',
   standalone: true,
   imports: [
-        MatCardModule,
-        MatIconModule,
-        MatDividerModule,
-        FontAwesomeModule,
-        PagetitleComponent,
-        ButtonFieldComponent,
-        FabButtonFieldComponent,
-        MatTooltipModule,
-        SearchComponent,
-        TableComponent,
-        ReactiveFormsModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    FontAwesomeModule,
+    PagetitleComponent,
+    ButtonFieldComponent,
+    FabButtonFieldComponent,
+    MatTooltipModule,
+    SearchComponent,
+    TableComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './adjustments.component.html',
-  styleUrl: './adjustments.component.scss'
+  styleUrl: './adjustments.component.scss',
 })
 export class AdjustmentsComponent {
-  getIcon=getIcon
+  getIcon = getIcon;
   Adjustment!: FormGroup;
   fileName = 'Adjustments';
-  actions = ['update']
+  actions = ['update'];
   sortCols = [
     'Param Id',
     'Adjustment Code',
     'Adjustment Name',
     'Adjustment Amount',
     'Parameter Version',
-    'Status',];
+    'Status',
+  ];
   columnsToExport = exportAdjustmentsData;
   params: any[] = [];
 
-  constructor(
-    private commonService: CommonService,
-    private exportService: ExportService,
-    private exportPdfService: ExportPdfService
-  ) {}
+  get formParameters() {
+    return this.getParameters.bind(this);
+  }
+  constructor(private commonService: CommonService) {}
   myTableData: {
     displayedColumns: string[];
     dataSource: MatTableDataSource<AdjustmentInterface>;
@@ -69,11 +67,9 @@ export class AdjustmentsComponent {
     this.getTableData();
   }
 
-  
-
   getTableData() {
     let responseData = [];
-    const response =AdjustmentsData;
+    const response = AdjustmentsData;
 
     response.data.map((item) => {
       let dataList = {};
@@ -92,36 +88,11 @@ export class AdjustmentsComponent {
     ];
   }
 
-  
   getParameters() {
     return this.params;
   }
 
-
   onSubmit() {
     console.log();
   }
-
-
-
-  onExcelClicked() {
-    this.exportService.exportToExcel(
-      this. myTableData[0].dataSource.data,
-      this.fileName,
-      this.columnsToExport,
-      this.getParameters()
-    );
-  }
-
-  onPdfClicked() {
-    this.exportPdfService.exportToPDF(
-      this. myTableData[0].dataSource.data,
-      this.fileName,
-      this.columnsToExport,
-      this.getParameters()
-    );
-  }
-  
 }
-  
-
